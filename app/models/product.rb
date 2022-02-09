4ef33a6fdf52eac14274522fb40aa059ea55ef8b
Aakanksha Patwa <aakankshapatwa36@gmail.com>
2022-02-09 21:08:48 +0530

    Successfully Integreted Stripe Checkout

---
+++ app/models/product.rb
@@ -0,0 +1,14 @@
+class Product < ApplicationRecord
+	validates :name, :price, presence: true
+
+	def to_s
+		name
+	end
+
+	after_create do
+        product = Stripe::Product.create(name: name)
+        price = Stripe::Price.create(product: product, unit_amount: self.price, currency: "inr")
+        update(stripe_product_id: product.id, stripe_price_id: price.id)
+    end
+   
+end
